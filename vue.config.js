755319cd3b35b987abc40982ace962a5357b0b65
Mounir <berniam.pro@gmail.com>
2022-06-25 12:29:52 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+});
+
+module.exports = {
   devServer: {
     disableHostCheck: true,
   },
-});
+};
